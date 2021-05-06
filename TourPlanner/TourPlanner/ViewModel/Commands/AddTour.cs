bf537ebae7c77448fc55ce36cc7154b9f336d839
lukas nowy <lukas.n912@gmail.com>
2021-05-06 21:18:43 +0200

    Add Tour Command implemented, Listbox Binding

---
+++ TourPlanner/TourPlanner/ViewModel/Commands/AddTour.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TourPlanner.Model;
+
+namespace TourPlanner.ViewModel.Commands
+{
+    class AddTour : ICommand
+    {
+        private MainViewModel _mainViewModel;
+
+        public AddTour(MainViewModel mainViewModel)
+        {
+            _mainViewModel = mainViewModel;
+
+            _mainViewModel.PropertyChanged += (sender, args) =>
+            {
+                Debug.Print("command: reveived prop changed");
+                if (args.PropertyName == "Tour")
+                {
+                    Debug.Print("command: reveived prop changed of Tour");
+                    CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+                }
+            };
+
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            Debug.Print("command: can execute");
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Debug.Print("command: add Tour");
+            Tour t = new Tour("New Tour", "Tour Description");
+            _mainViewModel.Tours.Add(t);
+            Debug.Print(_mainViewModel.Tours.Count.ToString());
+            Debug.Print("command: add Tour Done");
+
+        }
+    }
+}
--- TourPlanner/TourPlanner/ViewModel/MainViewModel.cs
+++ TourPlanner/TourPlanner/ViewModel/MainViewModel.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TourPlanner.Model;
+using TourPlanner.ViewModel.Commands;
 
 namespace TourPlanner.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public ObservableCollection<Tour> Tours { get; } = new ObservableCollection<Tour>();
+
+        public RelayCommand AddTourCommand { get; }
+
+        public MainViewModel()
+        {
+            AddTourCommand = new RelayCommand((_) =>
+            {
+                Tours.Add(new Tour("New Tour", "Tour Description"));
+                Debug.Write("Tour added");
+            });
+
+            //add some tours
+            Tours.Add(new Tour("Tour 1", "Description 1"));
+            Tours.Add(new Tour("Tour 2", "Description 2"));
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            Debug.Print($"propertyChanged \"{propertyName}\"");
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
