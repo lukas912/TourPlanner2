15d2575d19dd2462c8d6cda2ac094ab307d519b3
lukas nowy <lukas.n912@gmail.com>
2021-05-06 20:33:35 +0200

    Tour class added

---
+++ TourPlanner/TourPlanner/Model/Tour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.Model
+{
+    class Tour
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Tour(string t, string d)
+        {
+            this.Title = t;
+            this.Description = d;
+        }
+    }
+}
