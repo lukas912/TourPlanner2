ff70fa78c661d833f0872483ae7e52d4e5497e9f
lukas nowy <lukas.n912@gmail.com>
2021-05-10 20:26:45 +0200

    Tour Import & Tour Log import

---
+++ TourPlanner/TourPlanner/Model/TourLog_Import.cs
@@ -0,0 +1,40 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.Model
+{
+    class TourLog_Import
+    {
+        //Import TourLog from JSON
+        public List<TourLog> JSON_Import(string path)
+        {
+            string json = JsonConvert.DeserializeObject<string>(path);
+
+            JArray a = JArray.Parse(json);
+
+            IList<TourLog> tour_logs = a.ToObject<IList<TourLog>>();
+
+            return tour_logs.ToList();
+        }
+
+        //Import TourLog from CSV
+        public List<TourLog> CSV_Import(string path)
+        {
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<TourLog>();
+                return records.ToList();
+            }
+
+        }
+    }
+}
---
+++ TourPlanner/TourPlanner/Model/Tour_Import.cs
@@ -0,0 +1,37 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.Model
+{
+    class Tour_Import
+    {
+        //Import Tour from JSON
+        public Tour JSON_Import(string path)
+        {
+            // read file into a string and deserialize JSON to a type
+            Tour tour = JsonConvert.DeserializeObject<Tour>(File.ReadAllText(path));
+
+            return tour;
+        }
+
+        //Import Tour from CSV
+        public Tour CSV_Import(string path)
+        {
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Tour>();
+                return records.ElementAt(0);
+            }
+
+        }
+    }
+}
