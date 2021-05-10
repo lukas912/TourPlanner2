d82389c8123522fd0b2f11517da29f92a3e39b86
lukas nowy <lukas.n912@gmail.com>
2021-05-10 16:57:21 +0200

    Tour JSON Export implemented

---
+++ TourPlanner/TourPlanner/Model/Tour_Export.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TourPlanner.Model
+{
+    class Tour_Export
+    {
+        public void JSON_Export(Tour tour, string path)
+        {
+            // serialize JSON to a string and then write string to a file
+            File.WriteAllText(path, JsonConvert.SerializeObject(tour));
+        }
+    }
+}
