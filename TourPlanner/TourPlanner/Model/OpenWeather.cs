fdbed0c70117e682f39adb873987f9cba75e8367
lukas nowy <lukas.n912@gmail.com>
2021-05-13 22:46:54 +0200

    OpenWeather API implemented

---
+++ TourPlanner/TourPlanner/Model/OpenWeather.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TourPlanner.Model
+{
+    class OpenWeather
+    {
+        public string KEY = "49720b74d46a9b89d62da24abbce9400";
+        public string BASE_URL = "https://www.mapquestapi.com/staticmap/v5/map?start=";
+
+        public WeatherData getWeatherData(string city)
+        {
+            string url = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + KEY;
+
+            var client = new RestClient(url);
+
+            var request = new RestRequest();
+
+            var response = client.Get(request);
+
+            JObject obj = JObject.Parse(response.Content);
+
+
+            WeatherData wd = new WeatherData(city, (string)obj["weather"]["description"], (string)obj["main"]["temp"], (string)obj["main"]["humidity"], (string)obj["wind"]["speed"]);
+
+            return wd;
+        }
+    }
+}
---
+++ TourPlanner/TourPlanner/Model/WeatherData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.Model
+{
+    class WeatherData
+    {
+        public string City { get; set; }
+        public string Description { get; set; }
+        public string Temperature { get; set; }
+        public string Humidity { get; set; }
+        public string WindSpeed { get; set; }
+
+        public WeatherData(string city, string desc, string temp, string humid, string windspd)
+        {
+            this.City = city;
+            this.Description = desc;
+            this.Temperature = Convert.ToString(Convert.ToDouble(temp) - 273.15) + " °C";
+            this.Humidity = humid + " %";
+            this.WindSpeed = windspd + " km/h";
+            
+        }
+    }
+}
